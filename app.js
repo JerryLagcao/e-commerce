4d350acc95b27b3256760697c4756d395d65d6ec
JerryLagcao <70632301+JerryLagcao@users.noreply.github.com>
2023-03-25 15:26:02 +0800

    app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const productData = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 19.99,
+    image: 'https://via.placeholder.com/300x200',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget ex risus. Ut sed lorem vitae purus placerat dapibus.'
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 29.99,
+    image: 'https://via.placeholder.com/300x200',
+    description: 'Lorem ipsum dolor sit
